a950a2b3d72a11df86fdfb9eeb188e5109196389
Roman <s91630277@gmail.com>
2019-07-12 17:15:30 +0400

    feat: First release

---
+++ MarkdownLink.py
@@ -0,0 +1,12 @@
+import sublime
+import sublime_plugin
+# import os
+# import re
+# from .utils import *
+
+# view.run_command('markdown_link', args=({}))
+
+class MarkdownLinkCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        pass
